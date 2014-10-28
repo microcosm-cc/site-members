42cbe72cc4c059b4e82843615806de4d7938e2ec
David Kitchen <david@buro9.com>
2014-10-28 18:14:33 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-site-members
+Site Members
 ============
 
 Allows site admins to grant or deny access to a site based on the email address of the user.
+
+Just edit the config file so that the following values are set:
+
+`Subdomain` = the subdomain for a site, this is as per your Microcosm control panel, if your site was originally https://foo.microco.sm/ then your subdomain is `foo`. If you have a custom domain name, such as www.foobar.com, your subdomain is **still** `foo`.
+
+`Token` = an `access_token` for a privileged user on the site, either an administrator, site owner or moderator. If you are one of these people then you can view your cookie using Google Chrome when signed into the site by pressing F12 and then choosing Resources > Cookies. The access_token is a very long string.
+
+`IsMember` = a true|false value, if true it adds the person as a member, if false it removes them as a member
+
+`Emails` = a comma-delimited, "quoted" list of email addresses to add or remove
+
+An example:
+
+```
+{
+	"Subdomain": "foo",
+	"Token": "access_token",
+	"IsMember": true,
+	"Emails": [
+		"someone@example.com",
+		"someone-else@example.com"
+	]
+}
+```
+
+Then just:
+
+`go build && ./site-members`
---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type Conf struct {
+	Subdomain string
+	Token     string
+	IsMember  bool
+	Emails    []string
+}
+
+type Profiles struct {
+	Status int32 "json:`status`"
+	Data   []struct {
+		ProfileName string `json:"profileName"`
+		Meta        struct {
+			Links []struct {
+				Href string `json:"href"`
+			} `json:"links"`
+		} `json:"meta"`
+	} "json:`data`"
+	Error string "json:`error`"
+}
+
+func main() {
+	// Load config file
+	f, err := os.Open("config.json")
+	if err != nil {
+		fmt.Printf("Config file error: %v\n", err)
+		os.Exit(1)
+	}
+	d := json.NewDecoder(f)
+	conf := Conf{}
+	err = d.Decode(&conf)
+	if err != nil {
+		fmt.Printf("Config parsing error: %v\n", err)
+		f.Close()
+		os.Exit(1)
+	}
+	f.Close()
+
+	if conf.Subdomain == "" {
+		fmt.Errorf("Please set the 'Subdomain' in the config file to the subdomain of your Microcosm site.")
+		os.Exit(1)
+	}
+
+	if conf.Token == "" {
+		fmt.Errorf("Please set the 'Token' in the config file to the 'access_token' cookie of your signed in admin user or moderator.")
+		os.Exit(1)
+	}
+
+	if len(conf.Emails) == 0 {
+		fmt.Errorf("Please provide one or more 'Emails' in the config file.")
+		os.Exit(1)
+	}
+
+	// Fetch (or create) profile info for each email address
+	var emails string
+	for _, e := range conf.Emails {
+		emails += fmt.Sprintf(`{"email":"%s"}`, e)
+	}
+	emails = "[" + emails + "]"
+
+	req, err := http.NewRequest(
+		"POST",
+		"https://"+conf.Subdomain+".microco.sm/api/v1/users?access_token="+conf.Token,
+		bytes.NewBufferString(emails),
+	)
+	if err != nil {
+		fmt.Printf("Get user info request error: %v\n", err)
+		os.Exit(1)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Get user info error: %v\n", err)
+		os.Exit(1)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf(" = %d\n", resp.StatusCode)
+		os.Exit(1)
+	}
+
+	// Load profiles
+	d = json.NewDecoder(resp.Body)
+	p := Profiles{}
+	err = d.Decode(&p)
+	if err != nil {
+		fmt.Printf("Profiles parsing error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Set or unset the given var
+	for _, m := range p.Data {
+		url := "https://" + conf.Subdomain + ".microco.sm" + m.Meta.Links[0].Href + "/attributes/is_member?access_token=" + conf.Token
+
+		if conf.IsMember {
+			fmt.Print("Adding: " + m.ProfileName)
+			req, err = http.NewRequest("PUT", url, bytes.NewBufferString(`{"value": true}`))
+		} else {
+			fmt.Print("Removing: " + m.ProfileName)
+			req, err = http.NewRequest("DELETE", url, nil)
+		}
+		if err != nil {
+			fmt.Printf("Request error: %v\n", err)
+			os.Exit(1)
+		}
+		req.Header.Set("Content-Type", "application/json")
+
+		client := http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("Client error: %v\n", err)
+			os.Exit(1)
+		} else {
+			if resp.StatusCode == http.StatusOK {
+				fmt.Printf("OK")
+			} else {
+				if !conf.IsMember && resp.StatusCode == http.StatusNotFound {
+					fmt.Printf("OK")
+				}
+			}
+		}
+	}
+}
